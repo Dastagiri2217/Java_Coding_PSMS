c9aede503b0e8c34b089cdf8bf62c22423e30978
Dastagiri <162585700+Dastagiri2217@users.noreply.github.com>
2025-08-14 14:29:26 +0530

    Create code.java

---
+++ Day_8/code.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class BinarySearchRotated {
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {  // left half sorted
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {  // right half sorted
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int index = search(nums, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
